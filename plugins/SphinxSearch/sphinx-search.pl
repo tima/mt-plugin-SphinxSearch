9ad48e26f10d563fdcf4e50a663e93c911383d31
David Raynes <rayners@sixapart.com>
2006-12-14 17:42:05 +0000

    Added indexer task and config template

--- plugins/SphinxSearch/sphinx-search.pl
+++ plugins/SphinxSearch/sphinx-search.pl
@@ -7,6 +7,7 @@ use warnings;
 use base qw( MT::Plugin );
 
 use MT;
+use File::Spec;
 
 use vars qw( $VERSION $plugin );
 $VERSION = '0.1';
@@ -18,5 +19,49 @@ $plugin = MT::Plugin::SphinxSearch->new ({
         author_name => 'Apperceptive, LLC',
         author_link => 'http://www.apperceptive.com/',
 
-        });
+        system_config_template  => 'system_config.tmpl',
+        settings    => MT::PluginSettings->new ([
+            [ 'sphinx_path', { Default => undef, Scope => 'system' }],
+            [ 'sphinx_conf_path', { Default => undef, Scope => 'system' }],
+            ]),
+        
+        tasks   => {
+            'sphinx_indexer'    => {
+                name    => 'Sphinx Indexer',
+                frequency   => 60 * 60,
+                code        => \&sphinx_indexer_task,
+            }
+        },
+
+});
 MT->add_plugin ($plugin);
+
+sub sphinx_indexer_task {
+    my $task = shift;
+
+    my $sphinx_path = $plugin->get_config_value ('sphinx_path', 'system');
+    if (!$sphinx_path) {
+        my $app = MT->instance;
+        $app->log ('Sphinx path is not set');
+        return;
+    }
+
+    my $sphinx_conf = $plugin->get_config_value ('sphinx_conf_path',
+            'system');
+    if (!$sphinx_conf) {
+        my $app = MT->instance;
+        $app->log ('Sphinx conf path is not set');
+        return;
+    }
+    my $indexer_binary = File::Spec->catdir ($sphinx_path, 'indexer');
+    if (system ($indexer_binary, '--config', $sphinx_conf, '--all')) {
+        my $app = MT->instance;
+        if ($? == -1) {
+            $app->log ("Sphinx indexer failed to execute: $!");
+        }
+        else {
+            my $msg = sprintf "Sphinx indexer exited with value %d", $? >> 8;
+            $app->log ($msg);
+        }
+    }
+}
