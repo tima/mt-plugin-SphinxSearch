e78d8594aac5dd8481f8d5232a6d7e307f6c0ba7
David Raynes <rayners@sixapart.com>
2008-06-27 23:11:44 +0000

    Bumped version number

--- plugins/SphinxSearch/sphinx-search.pl
+++ plugins/SphinxSearch/sphinx-search.pl
@@ -14,7 +14,7 @@ use POSIX;
 use MT::Util qw( ts2epoch );
 
 use vars qw( $VERSION $plugin );
-$VERSION = '0.99.36';
+$VERSION = '0.99.37';
 $plugin = MT::Plugin::SphinxSearch->new ({
         name    => 'SphinxSearch',
         description => 'A search script using the sphinx search engine for MySQL',
