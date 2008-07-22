c2ff5618a1627523fe1a86c2768104ae359b75db
David Raynes <rayners@sixapart.com>
2008-07-22 20:29:02 +0000

    Merged revisions 9953,9955-9960,9962-9965 via svnmerge from http://soyluck.textdriven.com/svn/apperceptive/plugins/SphinxSearch/trunk
    ........
      r9953 | draynes | 2008-06-24 12:28:35 -0400 (Tue, 24 Jun 2008) | 1 line
      
      Added a bunch of pagination tags
    ........
      r9960 | draynes | 2008-06-24 14:34:01 -0400 (Tue, 24 Jun 2008) | 1 line
      
      Bunch of tag updates to take current context into consideration
    ........
      r9965 | draynes | 2008-06-24 14:59:54 -0400 (Tue, 24 Jun 2008) | 1 line
      
      Set max results to total number of possible entries
    ........

--- plugins/SphinxSearch/sphinx-search.pl
+++ plugins/SphinxSearch/sphinx-search.pl
@@ -75,6 +75,8 @@ sub init_registry {
                 'PreviousSearchResultsPage' => \&previous_search_results_page,              
 
                 'SearchAllResult'       => \&search_all_result_tag,
+
+                'SearchTotalPages'      => \&search_total_pages_tag,
             },
             block   => {
                 'IfCurrentSearchResultsPage?'    => \&if_current_search_results_page_conditional_tag,
@@ -83,6 +85,8 @@ sub init_registry {
                 'IfSingleSearchResultsPage?'     => sub { !if_multiple_search_results_pages_conditional_tag (@_) },
                 
                 'SearchResultsPageLoop'  => \&search_results_page_loop_container_tag,
+                'SearchNextPage'        => \&search_next_page_tag,
+                'SearchPreviousPage'    => \&search_previous_page_tag,
                 
                 'IfFirstSearchResultsPage?'  => sub { !previous_search_results_page (@_) },
                 'IfLastSearchResultsPage?'   => sub { !next_search_results_page (@_) },
@@ -317,6 +321,7 @@ sub straight_sphinx_search {
     
     my $offset = $app->param ('offset') || 0;
     my $limit  = $app->param ('limit') || $app->{searchparam}{MaxResults};
+    my $max    = MT::Entry->count ({ status => MT::Entry::RELEASE(), blog_id => \@blog_ids });
     
     my $match_mode = $app->param ('match_mode') || 'all';
     
@@ -327,6 +332,7 @@ sub straight_sphinx_search {
         Offset          => $offset, 
         Limit           => $limit,
         Match           => $match_mode,
+        Max             => $max,
     );
     my(%blogs, %hits);
     my $i = 0;
@@ -620,7 +626,8 @@ sub sphinx_search {
     }
     
     my $offset = 0;
-    my $limit = 200;
+    my $limit  = 200;
+    my $max    = 0;
     if (exists $params{Offset}) {
         $offset = $params{Offset};
     }
@@ -629,7 +636,11 @@ sub sphinx_search {
         $limit = $params{Limit};
     }
     
-    $spx->SetLimits ($offset, $limit);
+    if (exists $params{Max}) {
+        $max = $params{Max};
+    }
+    
+    $spx->SetLimits ($offset, $limit, $max);
     
     my $results = $spx->Query ($search, join ( ' ', map { my $ds = $_->datasource; $ds . '_index' . ( $indexes{$ds}->{delta} ? " ${ds}_delta_index" : '' ) } @classes ) );
     if (!$results) {
@@ -684,6 +695,9 @@ sub search_results_page_loop_container_tag {
         unshift @pages, $pages[0] - 1 if ($pages[0] > 1);    
         push @pages, $pages[$#pages] + 1 if ($pages[$#pages] < $number_pages);
     }
+    
+    local $ctx->{__stash}{sphinx_page_loop_first} = $pages[0];
+    local $ctx->{__stash}{sphinx_page_loop_last}  = $pages[$#pages];
     for my $page (@pages) {
         local $ctx->{__stash}{sphinx_page_number} = $page;
         # offset is 0 for page 1, limit for page 2, limit * 2 for page 3, ...
@@ -807,6 +821,86 @@ sub previous_search_results_page {
 sub search_all_result_tag {
     require MT::App;
     MT::App->instance->param ('searchall') ? 1 : 0;
+}
+
+sub search_total_pages_tag {
+    require MT::Request;
+    MT::Request->instance->stash ('sphinx_pages_number');
+}
+
+sub search_next_page_tag {
+    my ($ctx, $args, $cond) = @_;
+    require MT::Request;
+    my $r = MT::Request->instance;
+    my $current_page = $r->stash ('sphinx_pages_current');
+    my $number_pages = $r->stash ('sphinx_pages_number');
+    
+    return '' if ($current_page >= $number_pages);
+
+    my $page = $current_page + 1;
+    
+    my $limit   = $r->stash ('sphinx_pages_limit');
+    my $builder = $ctx->stash ('builder');
+    my $tokens  = $ctx->stash ('tokens');
+    
+    local $ctx->{__stash}{sphinx_page_number} = $page;
+    # offset is 0 for page 1, limit for page 2, limit * 2 for page 3, ...
+    local $ctx->{__stash}{sphinx_pages_offset} = ($page - 1) * $limit;
+    defined (my $out = $builder->build ($ctx, $tokens, {
+        %$cond,
+        IfCurrentSearchResultsPage => ($page == $current_page),
+    })) or return $ctx->error ($builder->errstr);
+    $out;
+}
+
+sub search_previous_page_tag {
+    my ($ctx, $args, $cond) = @_;
+    require MT::Request;
+    my $r = MT::Request->instance;
+    my $current_page = $r->stash ('sphinx_pages_current');
+    
+    return '' if ($current_page <= 1);
+
+    my $page = $current_page - 1;
+
+    my $limit   = $r->stash ('sphinx_pages_limit');
+    my $builder = $ctx->stash ('builder');
+    my $tokens  = $ctx->stash ('tokens');
+
+    local $ctx->{__stash}{sphinx_page_number} = $page;
+    # offset is 0 for page 1, limit for page 2, limit * 2 for page 3, ...
+    local $ctx->{__stash}{sphinx_pages_offset} = ($page - 1) * $limit;
+    defined (my $out = $builder->build ($ctx, $tokens, {
+        %$cond,
+        IfCurrentSearchResultsPage => ($page == $current_page),
+    })) or return $ctx->error ($builder->errstr);
+    $out;
+}
+
+sub if_first_search_results_page_conditional_tag {
+    my ($ctx, $args) = @_;
+    require MT::Request;
+    my $current_page = MT::Request->instance->stash ('sphinx_pages_current');
+    if (my $first = $ctx->stash ('sphinx_page_loop_first')) {
+        return $current_page == $first;
+    }
+    else {
+        return $current_page == 1;
+    }
+}
+
+sub if_last_search_results_page_conditional_tag {
+    my ($ctx, $args) = @_;
+    require MT::Request;
+    my $r = MT::Request->instance;
+    my $current_page = $r->stash ('sphinx_pages_current');
+    my $number_pages = $r->stash ('sphinx_pages_number');
+    if (my $last = $ctx->stash ('sphinx_page_loop_last')) {
+        return $current_page == $last;
+    }
+    else {
+        return $current_page == $number_pages;
+    }
 }
 
 1;
