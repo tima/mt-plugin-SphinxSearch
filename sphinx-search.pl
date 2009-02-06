4f4249f11541b065cc68ec527dacf160c8f7b4f6
David Raynes <rayners@sixapart.com>
2009-02-06 15:11:07 +0000

    Bumped version number

--- sphinx-search.pl
+++ sphinx-search.pl
@@ -12,7 +12,7 @@ use File::Spec;
 use POSIX;
 
 use vars qw( $VERSION $plugin );
-$VERSION = '0.99.55mt4';
+$VERSION = '0.99.56mt4';
 $plugin  = MT::Plugin::SphinxSearch->new(
     {
         id   => 'SphinxSearch',
