1020c5011f1605a66d7e95b6a5340e9c8b0ba51f
David Raynes <rayners@sixapart.com>
2009-02-20 20:30:56 +0000

    Store the sphinx object in MT::Request, so it dies and goes away after each request.

--- lib/SphinxSearch/Util.pm
+++ lib/SphinxSearch/Util.pm
@@ -1,13 +1,15 @@
 
 package SphinxSearch::Util;
 
-my $spx;
+use MT::Request;
+# my $spx;
 
 sub _reset_sphinx {
-    undef $spx;
+    # undef $spx;
 }
 
 sub _get_sphinx {
+    my $spx = MT::Request->instance->stash ('sphinx_obj');
     require Sphinx;
     return $spx if $spx;
     $spx = Sphinx->new;
@@ -17,12 +19,13 @@ sub _get_sphinx {
         $plugin->get_config_value( 'searchd_host', 'system' ),
         $plugin->get_config_value( 'searchd_port', 'system' )
     );
-
+    MT::Request->instance->stash ('sphinx_obj', $spx);
     return $spx;
 }
 
 sub _get_sphinx_error {
-    return unless $spx;
+    # return unless $spx;
+    my $spx = _get_sphinx();
     $spx->GetLastError();
 }
 
