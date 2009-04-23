1d0618bb1ec1a9e7ad1dcd4403c825c0aaa177b4
David Raynes <rayners@sixapart.com>
2009-04-23 20:40:55 +0000

    Incorporating some fixes.

--- plugins/SphinxSearch/lib/SphinxSearch/Config.pm
+++ plugins/SphinxSearch/lib/SphinxSearch/Config.pm
@@ -184,18 +184,4 @@ sub _gen_sphinx_conf_tmpl {
     $tmpl;
 }
 
-sub gen_sphinx_conf {
-    my $app    = shift;
-    my $plugin = MT->component('sphinxsearch');
-    my $tmpl   = $plugin->_gen_sphinx_conf_tmpl;
-
-    my $str = $app->build_page($tmpl);
-    die $app->errstr if ( !$str );
-    $app->{no_print_body} = 1;
-    $app->set_header(
-        "Content-Disposition" => "attachment; filename=sphinx.conf" );
-    $app->send_http_header('text/plain');
-    $app->print($str);
-}
-
 1;
