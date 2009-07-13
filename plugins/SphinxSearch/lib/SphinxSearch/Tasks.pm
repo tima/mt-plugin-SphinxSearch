67550d5f81b8fd96090d3747d4fc2f31617d7687
David Raynes <rayners@sixapart.com>
2009-07-13 18:29:06 +0000

    Cleaned up some debug code

--- plugins/SphinxSearch/lib/SphinxSearch/Tasks.pm
+++ plugins/SphinxSearch/lib/SphinxSearch/Tasks.pm
@@ -9,9 +9,7 @@ sub sphinx_delta_indexer {
 }
 
 sub sphinx_indexer {
-    print "pre indexer task call\n";
     sphinx_indexer_task( 'main', @_ );
-    print "post indexer task call\n";
 }
 
 sub sphinx_indexer_task {
@@ -19,13 +17,10 @@ sub sphinx_indexer_task {
     my $which  = shift;
     my $task   = shift;
 
-    print "pre config check\n";
     return
       unless $plugin->get_config_value( 'use_indexer_tasks', 'system' )
           && MT->config->UseSphinxTasks;
     
-    print "past config check\n";
-
     require MT::TheSchwartz;
     require TheSchwartz::Job;
 
@@ -34,8 +29,8 @@ sub sphinx_indexer_task {
     $job->uniqkey($which);
     $job->priority(10)
       ; # reindexing is high priority, it should be delayed as little as possible
-    MT::TheSchwartz->insert($job) or die MT::TheSchwartz->errstr;
-
+    MT::TheSchwartz->insert($job);
+    
     1;
 }
 
